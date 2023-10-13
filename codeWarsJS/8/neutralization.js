a9db221bb39c64eb641c35e6d2299786b83c5c7c
AgileMyk <90538513+AgileMyk@users.noreply.github.com>
2023-10-13 17:29:49 -0400

    nautralization; ternary form

--- codeWarsJS/8/neutralization.js
+++ codeWarsJS/8/neutralization.js
@@ -26,3 +26,11 @@ function neutralise(s1, s2) {
     }
     return s3;
   }
+
+  function neutralise(s1, s2) {
+    let res = "";
+    for(let i = 0; i < s1.length; i++) {
+      res += s1[i] !== s2[i]? "0": s1[i];
+    }
+    return res;
+  }
