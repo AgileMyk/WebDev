2b5b2370d8db3e473838c530f32f406e12fdae89
AgileMyk <90538513+AgileMyk@users.noreply.github.com>
2023-11-09 19:30:10 -0500

    finished playerSelection() first RPS prog

--- OdinProject/rockPaperScissors.js
+++ OdinProject/rockPaperScissors.js
@@ -6,7 +6,10 @@ const getComputerChoice = function() {
 }
 
 const playerSelection = function() {
-
+    Rock = /rock/i;
+    Paper = /paper/i;
+    Scissors = /scissors/i;
+    let result;
     while (!Rock.test(result) && !Paper.test(result) && !Scissors.test(result)) { 
         let selection = prompt("Please make your choice (rock, paper, or scissors");
         if (Rock.test(selection)) {         
@@ -15,7 +18,9 @@ const playerSelection = function() {
             result = 'Paper';     
         } else if (Scissors.test(selection)) {         
             result = 'Scissors';     
-        }     
+        } else {
+            return playerSelection();
+        }
         return result; 
     }
 }
